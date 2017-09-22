3211339d9965731f612a57f86608a4e63987b8b1
James Burgess <jamesmillerburgess@gmail.com>
2017-09-22 22:29:56 +0200

    Reset login mode on logout Fixes #12

--- imports/components/nav/nav/NavConnect.js
+++ imports/components/nav/nav/NavConnect.js
@@ -4,6 +4,8 @@ import { Meteor } from 'meteor/meteor';
 import NavContainer from './NavContainer';
 
 import { setNavProp } from '../../../state/actions/navActionCreators';
+import { setLoginProp } from '../../../state/actions/loginActionCreators';
+import { LOGIN } from '../../pages/home/HomeConstants';
 
 export const mapStateToProps = ({ nav }) => ({
   isProfileMenuOpen: nav.isProfileMenuOpen || false,
@@ -13,7 +15,11 @@ export const mapDispatchToProps = (dispatch, ownProps) => ({
   setIsProfileMenuOpen: value =>
     dispatch(setNavProp('isProfileMenuOpen', value)),
   goToProfile: () => ownProps.history.push('/profile'),
-  tryLogout: () => Meteor.logout(() => ownProps.history.push('/')),
+  tryLogout: () =>
+    Meteor.logout(() => {
+      dispatch(setLoginProp('mode', LOGIN));
+      ownProps.history.push('/');
+    }),
 });
 
 const NavConnect = connect(mapStateToProps, mapDispatchToProps)(NavContainer);
