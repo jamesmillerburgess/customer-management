621e36abf7c319d0c9ad560375359d2b8d75318b
James Burgess <jamesmillerburgess@gmail.com>
2017-09-23 22:41:22 +0200

    More styling of overlay

--- imports/components/overlays/addCompany/AddCompanyDisplay.js
+++ imports/components/overlays/addCompany/AddCompanyDisplay.js
@@ -2,27 +2,31 @@ import React from 'react';
 
 const AddCompanyDisplay = props => (
   <div className="overlay-right">
-    <div className="overlay-header">
+    <header className="overlay-header">
       Add a company
       <button className="button-dismiss-overlay" onClick={props.closeOverlay}>
         X
       </button>
-    </div>
+    </header>
     <div className="overlay-body">
-      <div className="input-group">
-        <div className="input-label">Name</div>
-        <input />
-      </div>
-      <div className="input-group">
-        <div className="input-label">Website</div>
-        <input />
+      <div className="overlay-content">
+        <div className="input-group">
+          <div className="input-label">Name</div>
+          <input />
+        </div>
+        <div className="input-group">
+          <div className="input-label">Website</div>
+          <input />
+        </div>
       </div>
     </div>
-    <div className="overlay-footer">
+    <footer className="overlay-footer">
       <button className="button-primary">Create company</button>
       <button className="button-primary">Create and add another</button>
-      <button className="button-secondary">Cancel</button>
-    </div>
+      <button className="button-secondary" onClick={props.closeOverlay}>
+        Cancel
+      </button>
+    </footer>
   </div>
 );
 
