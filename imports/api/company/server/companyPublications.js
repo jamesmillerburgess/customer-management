f2080752f646ff57adaed55317a613a4f3ca40d5
James Burgess <jamesmillerburgess@gmail.com>
2017-09-25 22:02:45 +0200

    Cover companyPublications

--- imports/api/company/server/companyPublications.js
+++ imports/api/company/server/companyPublications.js
@@ -2,21 +2,21 @@ import { Meteor } from 'meteor/meteor';
 
 import Companies from '../companyCollection';
 
-export const publishUserCompanies = function() {
-  if (this.userId) {
-    return Companies.find({ users: this.userId, isArchived: false });
+export const user = function() {
+  if (!this.userId) {
+    throw new Error('Cannot subscribe without being logged in');
   }
-  return null;
+  return Companies.find({ users: this.userId, isArchived: false });
 };
 
-export const publishSingleCompany = function(companyId) {
-  if (this.userId) {
-    return Companies.find({ _id: companyId });
+export const single = function(companyId) {
+  if (!this.userId) {
+    throw new Error('Cannot subscribe without being logged in');
   }
-  return null;
+  return Companies.find({ _id: companyId });
 };
 
 Meteor.publish({
-  'company.user': publishUserCompanies,
-  'company.single': publishSingleCompany,
+  'company.user': user,
+  'company.single': single,
 });
