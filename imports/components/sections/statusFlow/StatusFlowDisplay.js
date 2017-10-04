586ff0120639dd23de034bebeb470eb5f2496f7c
James Burgess <jamesmillerburgess@gmail.com>
2017-10-05 00:18:52 +0530

    Fix icon placement

--- imports/components/sections/statusFlow/StatusFlowDisplay.js
+++ imports/components/sections/statusFlow/StatusFlowDisplay.js
@@ -11,26 +11,35 @@ const StatusFlowDisplay = props => (
         onClick={() => props.updateStatus(status.value)}
       >
         <div className="status-label">{status.label}</div>
-        <div className="move-to-label">Move to: {status.label}</div>
         <div className="icons">
+          <div
+            className="bar left"
+            style={{
+              backgroundColor:
+                index === 0
+                  ? 'transparent'
+                  : index <= props.statusIndex ? status.color : '',
+            }}
+          />
           <div
             className={`icon fa fa-fw ${index <= props.statusIndex
               ? 'fa-check-circle'
               : 'fa-circle-thin'}`}
-            style={{ color: index <= props.statusIndex ? status.color : '' }}
+            style={{
+              color: index <= props.statusIndex ? status.color : '',
+            }}
+          />
+          <div
+            className="bar right"
+            style={{
+              backgroundColor:
+                index === props.statuses.length - 1
+                  ? 'transparent'
+                  : index < props.statusIndex ? status.color : '',
+            }}
           />
         </div>
-        <div
-          className="status-node-connector"
-          style={{
-            background:
-              index < props.statusIndex
-                ? `linear-gradient(${status.color}, ${props.statuses[index + 1]
-                    .color})`
-                : '',
-            opacity: index === props.statuses.length - 1 ? '0' : '1',
-          }}
-        />
+        <div className="move-to-label">Move to: {status.label}</div>
       </button>
     ))}
   </div>
