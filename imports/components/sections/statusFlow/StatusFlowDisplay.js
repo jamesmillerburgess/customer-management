c9e7dc3336cb9bc85d963cace183001e887017aa
James Burgess <jamesmillerburgess@gmail.com>
2017-10-05 00:38:34 +0530

    Fix complex arrow function and some overlap issues

--- imports/components/sections/statusFlow/StatusFlowDisplay.js
+++ imports/components/sections/statusFlow/StatusFlowDisplay.js
@@ -2,46 +2,54 @@ import React from 'react';
 
 const StatusFlowDisplay = props => (
   <div className="status-flow">
-    {props.statuses.map((status, index) => (
-      <button
-        className={`status-node ${status.value === props.status
-          ? 'active'
-          : ''}`}
-        key={status.value}
-        onClick={() => props.updateStatus(status.value)}
-      >
-        <div className="status-label">{status.label}</div>
-        <div className="icons">
-          <div
-            className="bar left"
-            style={{
-              backgroundColor:
-                index === 0
-                  ? 'transparent'
-                  : index <= props.statusIndex ? status.color : '',
-            }}
-          />
-          <div
-            className={`icon fa fa-fw ${index <= props.statusIndex
-              ? 'fa-check-circle'
-              : 'fa-circle-thin'}`}
-            style={{
-              color: index <= props.statusIndex ? status.color : '',
-            }}
-          />
-          <div
-            className="bar right"
-            style={{
-              backgroundColor:
-                index === props.statuses.length - 1
-                  ? 'transparent'
-                  : index < props.statusIndex ? status.color : '',
-            }}
-          />
-        </div>
-        <div className="move-to-label">Move to: {status.label}</div>
-      </button>
-    ))}
+    {props.statuses.map((status, index) => {
+      const activeClass = status.value === props.status ? 'active' : '';
+      let leftColor = '';
+      let iconColor = '';
+      let rightColor = '';
+      if (index <= props.statusIndex) {
+        leftColor = status.color;
+        iconColor = status.color;
+      }
+      if (index < props.statusIndex) {
+        rightColor = status.color;
+      }
+      return (
+        <button
+          className={`status-node ${activeClass}`}
+          key={status.value}
+          onClick={() => props.updateStatus(status.value)}
+        >
+          <div className="status-label">{status.label}</div>
+          <div className="icons">
+            <div
+              className="bar left"
+              style={{
+                backgroundColor: index === 0 ? 'transparent' : leftColor,
+              }}
+            />
+            <div
+              className={`icon fa fa-fw ${index <= props.statusIndex
+                ? 'fa-check-circle'
+                : 'fa-circle-thin'}`}
+              style={{
+                color: iconColor,
+              }}
+            />
+            <div
+              className="bar right"
+              style={{
+                backgroundColor:
+                  index === props.statuses.length - 1
+                    ? 'transparent'
+                    : rightColor,
+              }}
+            />
+          </div>
+          <div className="move-to-label">Move to: {status.label}</div>
+        </button>
+      );
+    })}
   </div>
 );
 
