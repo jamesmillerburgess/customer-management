6f7a2bec1e0452102930b809720e4bedde02756a
James Burgess <jamesmillerburgess@gmail.com>
2017-10-07 10:33:38 +0200

    CC on Contact.js

--- imports/components/pages/contact/Contact.test.js
+++ imports/components/pages/contact/Contact.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import Contact from './Contact';
+import Contact, { contactProps } from './Contact';
+import FieldLists from '../../../api/fieldList/fieldListCollection';
 
 describe('Contact Component', () => {
   let wrapper;
@@ -13,4 +14,11 @@ describe('Contact Component', () => {
     wrapper = shallow(<Contact {...props} />, options);
   });
   it('renders without error', () => {});
+});
+describe('contactProps Function', () => {
+  it('uses the fieldList document if it finds one', () => {
+    const fieldList = { fields: { a: 'a' } };
+    FieldLists.docs = [fieldList];
+    expect(contactProps().properties).toEqual(fieldList.fields);
+  });
 });
