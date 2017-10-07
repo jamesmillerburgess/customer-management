4668776478d405380db8177ce1cc5e4adfb6d6c1
James Burgess <jamesmillerburgess@gmail.com>
2017-10-07 05:21:20 +0200

    Extend config fixtures

--- imports/startup/server/fixtures/defaults.js
+++ imports/startup/server/fixtures/defaults.js
@@ -3,6 +3,71 @@ export const FIELD_OPTIONS = [
     type: 'OPPORTUNITY_STATUS',
     options: [
       { value: 'APPOINTMENT_SCHEDULED', label: 'Appointment Scheduled' },
+      { value: 'QUALIFIED_TO_BUY', label: 'Qualified to Buy' },
+      { value: 'PRESENTATION_SCHEDULED', label: 'Presentation Scheduled' },
+      { value: 'DECISION_MAKER_BOUGHT_IN', label: 'Decision Maker Bought-In' },
+      { value: 'CONTRACT_SENT', label: 'Contract Sent' },
+      { value: 'CLOSED_WON', label: 'Closed Won' },
+      { value: 'CLOSED_LOST', label: 'Closed Lost' },
+    ],
+  },
+  {
+    type: 'CONTACT_LEAD_STATUS',
+    options: [
+      { value: 'LEAD', label: 'Lead' },
+      { value: 'MARKETING_QUALIFIED', label: 'Marketing Qualified' },
+      { value: 'SALES_QUALIFIED', label: 'Sales Qualified' },
+    ],
+  },
+  {
+    type: 'CONTACT_LIFECYCLE_STAGE',
+    options: [],
+  },
+];
+
+export const FIELD_LISTS = [
+  {
+    page: 'ADD_CONTACT',
+    fields: [
+      { prop: 'name', label: 'Name', type: 'TEXT', default: '' },
+      { prop: 'email', label: 'Email', type: 'TEXT', default: '' },
+      {
+        prop: 'company',
+        label: 'Company',
+        type: 'COMPANY',
+        default: null,
+      },
+    ],
+  },
+  {
+    page: 'CONTACT_PROPERTIES',
+    fields: [
+      { name: 'name', label: 'Name', fieldType: 'TEXT', default: '' },
+      { name: 'email', label: 'Email', fieldType: 'TEXT', default: '' },
+      {
+        name: 'company',
+        label: 'Company',
+        fieldType: 'COMPANY',
+        default: null,
+      },
+      {
+        name: 'phoneNumber',
+        label: 'Phone Number',
+        fieldType: 'TEXT',
+        default: '',
+      },
+      {
+        name: 'lifecycleStage',
+        label: 'Lifecycle Stage',
+        fieldType: 'TEXT',
+        default: '',
+      },
+      {
+        name: 'leadStatus',
+        label: 'Lead Status',
+        type: 'TEXT',
+        default: '',
+      },
     ],
   },
 ];
--- imports/startup/server/fixtures/fixtures.js
+++ imports/startup/server/fixtures/fixtures.js
@@ -2,7 +2,9 @@ import FieldOptions from '../../../api/fieldOptions/fieldOptionsCollection.js';
 import * as DEFAULTS from './defaults';
 const runFixtures = () => {
   if (!FieldOptions.findOne()) {
-    FieldOptions.insertMany(DEFAULT_FIELD_OPTIONS);
+    DEFAULTS.FIELD_OPTIONS.forEach(fieldOptions =>
+      FieldOptions.insert(fieldOptions)
+    );
   }
 };
 
