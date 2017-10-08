1d862c6c66e74f2f2e18fd6c9810b173206d0e3c
James Burgess <jamesmillerburgess@gmail.com>
2017-10-08 07:17:15 +0200

    Remove duplicate code from listpage entry points

--- imports/components/pages/companyList/CompanyList.js
+++ imports/components/pages/companyList/CompanyList.js
@@ -1,46 +1,15 @@
 import React from 'react';
-import moment from 'moment';
-import { Link } from 'react-router-dom';
 
-import ListPageConnect from '../listPage/ListPageConnect';
+import ListPageConnect, {
+  generateListPageProps,
+} from '../listPage/ListPageConnect';
 import Companies from '../../../api/company/companyCollection';
 
-export const companyListProps = {
-  subscription: 'company.user',
-  collection: Companies,
-  title: 'Companies',
-  searchPlaceholder: 'Search for a company',
-  addButtonText: 'Add company',
-  gridPageProps: companies => ({
-    sidebarHeader: 'All companies',
-    noRows: 'No companies yet!',
-    columns: [
-      {
-        width: 45,
-        resizable: false,
-        sortable: false,
-      },
-      {
-        Header: 'Name',
-        id: 'name',
-        accessor: 'name',
-        Cell: props => (
-          <Link to={`/companies/${companies[props.index]._id}`}>
-            {props.value}
-          </Link>
-        ),
-      },
-      {
-        Header: 'Create Date',
-        id: 'lastName',
-        accessor: 'createDate',
-        Cell: props => (
-          <span>{moment(props.value).format('MMM DD[,] YYYY')}</span>
-        ),
-      },
-    ],
-  }),
-};
+export const companyListProps = generateListPageProps(
+  'company',
+  'companies',
+  Companies
+);
 
 const CompanyList = props => (
   <ListPageConnect {...props} {...companyListProps} />
--- imports/components/pages/contactList/ContactList.js
+++ imports/components/pages/contactList/ContactList.js
@@ -1,46 +1,15 @@
 import React from 'react';
-import moment from 'moment';
-import { Link } from 'react-router-dom';
 
-import ListPageConnect from '../listPage/ListPageConnect';
+import ListPageConnect, {
+  generateListPageProps,
+} from '../listPage/ListPageConnect';
 import Contacts from '../../../api/contact/contactCollection';
 
-export const contactListProps = {
-  subscription: 'contact.user',
-  collection: Contacts,
-  title: 'Contacts',
-  searchPlaceholder: 'Search for a contact',
-  addButtonText: 'Add contact',
-  gridPageProps: contacts => ({
-    sidebarHeader: 'All contacts',
-    noRows: 'No contacts yet!',
-    columns: [
-      {
-        width: 45,
-        resizable: false,
-        sortable: false,
-      },
-      {
-        Header: 'Name',
-        id: 'name',
-        accessor: 'name',
-        Cell: props => (
-          <Link to={`/contacts/${contacts[props.index]._id}`}>
-            {props.value}
-          </Link>
-        ),
-      },
-      {
-        Header: 'Create Date',
-        id: 'lastName',
-        accessor: 'createDate',
-        Cell: props => (
-          <span>{moment(props.value).format('MMM DD[,] YYYY')}</span>
-        ),
-      },
-    ],
-  }),
-};
+export const contactListProps = generateListPageProps(
+  'contact',
+  'contacts',
+  Contacts
+);
 
 const ContactList = props => (
   <ListPageConnect {...props} {...contactListProps} />
