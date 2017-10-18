ac805ca265173b6c916c0c3aedcc204b8ef46b45
James Burgess <jamesmillerburgess@gmail.com>
2017-10-18 21:22:24 +0200

    Quick and dirty fix to missing properties Fixes #65

--- imports/components/pages/objectEditor/ObjectEditorContainer.js
+++ imports/components/pages/objectEditor/ObjectEditorContainer.js
@@ -1,27 +1,37 @@
 import { Meteor } from 'meteor/meteor';
 import { createContainer } from 'meteor/react-meteor-data';
 
+import FieldLists from '../../../api/fieldList/fieldListCollection';
+
 export const linkMeteorData = props => {
   if (!Meteor.userId()) {
     return { ...props, object: { timeline: [] }, loading: true };
   }
   const objectId = props.match.params.objectId;
   const loading = !Meteor.subscribe(props.subscription, objectId).ready();
+  const fieldList = FieldLists.findOne({ page: props.propertiesPage });
+  let properties = [];
+  if (fieldList && fieldList.fields) {
+    properties = fieldList.fields;
+  }
   const object = props.collection.findOne(objectId) || {
     name: '',
     timeline: [],
   };
   if (
     object._id &&
+    fieldList &&
+    fieldList.fields &&
     (!props.hasLoaded || object._id !== props.loadedValues._id)
   ) {
     props.setHasLoaded(true);
-    props.properties.forEach(properties =>
+    fieldList.fields.forEach(properties =>
       props.setProperty(properties.name, object[properties.name])
     );
+    properties = fieldList.fields;
     props.setLoadedValues(object);
   }
-  return { ...props, object, loading };
+  return { ...props, object, loading, properties };
 };
 
 const ObjectEditorContainer = ObjectEditorDisplay =>
